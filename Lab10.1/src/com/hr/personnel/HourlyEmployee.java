d219cb8a337674559a1606ddf10856dcce7cbff4
Daiyron Williams <DaiyronW@tlgcohort.com>
2024-05-30 16:18:52 -0500

    Lab10.1 complete - throw and catch checked exception

--- Lab10.1/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1/src/com/hr/personnel/HourlyEmployee.java
@@ -8,6 +8,8 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
@@ -26,7 +28,7 @@ public class HourlyEmployee extends Employee {
     }
 
     public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours)
-    throws IllegalArgumentException {
+    throws IllegalWageException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -51,9 +53,9 @@ public class HourlyEmployee extends Employee {
     /*
      * Throws IllegalArgumentException if incoming rate is less than the minimum
      */
-    public void setRate(double rate) throws IllegalArgumentException {
+    public void setRate(double rate) throws IllegalWageException {
         if (rate < FEDERAL_MINIMUM_WAGE) {
-            throw new IllegalArgumentException(
+            throw new IllegalWageException(
                     String.format("Illegal wage: %s. Federal minimum wage is %s.\n",
                             rate, FEDERAL_MINIMUM_WAGE));
         }
